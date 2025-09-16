9600bfb2a1a3f6f91e2617417ce931e519ffadbd
Md-Refat-Hosain <thisismyfiverrac@gmail.com>
2025-09-16 15:40:28 +0600

    Update custom_filters.py

--- dashboard/templatetags/custom_filters.py
+++ dashboard/templatetags/custom_filters.py
@@ -4,4 +4,6 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key)
+    if dictionary and key in dictionary:
+        return dictionary.get(key)
+    return ""
